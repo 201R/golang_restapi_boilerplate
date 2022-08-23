2ca95639fa0e878df9331e5046d8e08be32d25bb
Kaanari <kaanari@192.168.1.6>
2022-08-23 23:13:25 +0000

    init project structiure

--- main.go
+++ main.go
@@ -1,5 +1,46 @@
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/201R/golang_restapi_boilerplate/packages/logger"
+	"github.com/201R/golang_restapi_boilerplate/packages/setting"
+	"github.com/201R/golang_restapi_boilerplate/router"
+	"github.com/gin-gonic/gin"
+	_ "github.com/lib/pq"
+)
+
 func main() {
-	print("Let start conding...")
+	setting.Setup()
+	closeLogFile, err := logger.Setup()
+	if err != nil {
+		logger.Fatal(err)
+	}
+	defer closeLogFile()
+
+	gin.SetMode(setting.ServerSetting.RunMode)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		logger.Warnf("Defaulting to port %s", port)
+	}
+
+	routersInit := router.InitRouter()
+	readTimeout := setting.ServerSetting.ReadTimeout
+	writeTimeout := setting.ServerSetting.WriteTimeout
+	endPoint := fmt.Sprintf(":%s", port)
+
+	server := &http.Server{
+		Addr:         endPoint,
+		Handler:      routersInit,
+		ReadTimeout:  readTimeout,
+		WriteTimeout: writeTimeout,
+	}
+
+	logger.Infof("[info] start http server listening %s", endPoint)
+
+	server.ListenAndServe()
 }
--- router/router.go
+++ router/router.go
@@ -1,1 +1,11 @@
 package router
+
+import "github.com/gin-gonic/gin"
+
+func InitRouter() *gin.Engine {
+	r := gin.New()
+	r.Use(gin.Logger())
+	r.Use(gin.Recovery())
+
+	return r
+}
