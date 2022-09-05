af56535eaa718c8a8604277f96c7b4f8d0a6b8ec
201RichK <amosamospascal@yahoo.com>
2022-09-05 09:10:09 +0000

    repository/user.go

--- repository/user.go
+++ repository/user.go
@@ -1,1 +1,14 @@
 package repository
+
+import "github.com/201R/go_api_boilerplate/ent"
+
+// User repos from ent database
+type UserRepository struct {
+	client *ent.UserClient
+}
+
+func NewUserRepository(client *ent.UserClient) UserRepository {
+	return UserRepository{
+		client: client,
+	}
+}
